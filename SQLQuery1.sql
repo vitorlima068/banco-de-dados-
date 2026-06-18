a0a8b65ec2fb4160841cf128a6df2eb9a5237666
vitorlima068 <vitor.g.lima18@aluno.senai.br>
2026-06-18 10:46:17 -0500

    updt

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,8 +1,8 @@
 --1. Criar banco de dados--
-CREATE DATABASE EscolaDB;
+CREATE DATABASE ClinicaDB;
 
 --2. Selecionar o banco--
-USE EscolaDB;
+USE ClinicaDB;
 
 --3. Criar a tabela de turmas--
 CREATE TABLE Turmas(
@@ -20,4 +20,23 @@ CREATE TABLE alunos(
     -- criando o relacionamento --
     FOREIGN KEY (turma_id) REFERENCES Turmas(id)
 );
+
+--5. Adicionsr itens nas tabelas (ddl)--
+INSERT INTO Turmas (nome, turno) VALUES ('Progrmador Full-Stack', 'Manhã');
+INSERT INTO Turmas (nome, turno) VALUES ('Progrmador Full-Stack', 'Tarde');
+
+INSERT INTO Turmas (nome, email, turma_id) VALUES ('vitor', 'vitor.g.lima18@aluno.senai.br', 1);
+INSERT INTO Turmas (nome, email, turma_id) VALUES ('gabriel', 'vgzinn777vitorgabi@gmail.com', 1);
+INSERT INTO Turmas (nome, email, turma_id) VALUES ('ryan', 'ryan@docente.com', 1);
+
+--6. Mostrar as duas tabelas (juntas)--
+SELECT
+ Alunos.nome AS 'Nome do Aluno'
+ Alunos.email AS 'E-MAIL'
+ *FROM Alunos
+
+SELECT 
+ Turmas.nome AS 'Nome da Turma'
+ Turmas.turno AS 'turno'
+ *FROM Turmas
 
